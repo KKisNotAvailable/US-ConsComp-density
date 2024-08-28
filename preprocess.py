0b9a886c962ba912b16259021300866a5852fc03
KYK <ken19505@gmail.com>
2024-08-28 17:16:20 +0800

    started to get data from deed files.

--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,125 @@
 import pandas as pd
 import numpy as np
+import os
+
+
+PROPERTY_INDICATOR = {
+    10: "Single Family Residence / Townhouse",
+    11: "Condominium (residential)",
+    20: "Commercial",
+    21: "Duplex, Triplex, Quadplex)",
+    22: "Apartment",
+    23: "Hotel, Motel",
+    24: "Commercial (condominium)",
+    25: "Retail",
+    26: "Service (general public)",
+    27: "Office Building",
+    28: "Warehouse",
+    29: "Financial Institution",
+    30: "Hospital (medical complex, clinic)",
+    31: "Parking",
+    32: "Amusement-Recreation",
+    50: "Industrial",
+    51: "Industrial Light",
+    52: "Industrial Heavy",
+    53: "Transport",
+    54: "Utilities",
+    70: "Agricultural",
+    80: "Vacant",
+    90: "Exempt",
+    0: "Miscellaneous"
+}
 
 class Preprocess():
-    def __init__(self, filename: None) -> None:
-        self._filename = filename
+    def __init__(self, filepath: None) -> None:
+        self._filepath = filepath
 
-    def read_n_clean(self, cols_to_keep: list = []):
+    def _read_n_clean(self, filename: str = None, cols_to_keep: list = []) -> pd.DataFrame:
         '''
-        The actions done here will be based on the data type of self.__filename
-        str: does on single file
-        list: does on the list of files
-        None: all of the files in the directory
+        Based on the filename and cols_to_keep provided, will return
+        the desired dataframe.
+        Notice that this method does not filter the data.
         '''
+        if not filename:
+            print("Please provide valid file.")
+            return
+        
+        filename = self._filepath + filename
 
-        df = pd.read_csv(self._filename, delimiter="|")
+        df = pd.read_csv(filename, delimiter="|")
 
         if cols_to_keep:
             df = df[cols_to_keep]
 
-
         return df
 
-    def deed_analysis(self):
+    def deed_files(self, files = None):
+        '''
+        The actions done here will be based on the data type of "files
+        str: on single file
+        list: on the list of files
+        None: all of the files in the directory
+        '''
+        cols_to_keep = [
+            "PCL_ID_IRIS_FRMTD", # unique key
+            "BLOCK LEVEL LATITUDE",
+            "BLOCK LEVEL LONGITUDE",
+            "SITUS_CITY",
+            "SITUS_STATE",
+            "SITUS_ZIP_CODE",
+            "SELLER NAME1",
+            "SALE AMOUNT",
+            "SALE DATE",
+            "RECORDING DATE",
+            "PROPERTY_INDICATOR", # residential, commercial, ...
+            "RESALE/NEW_CONSTRUCTION" # M: re-sale, N: new construction
+        ]
+
+        def simple_analysis(df: pd.DataFrame):
+            cond_is_new = df['RESALE/NEW_CONSTRUCTION'] == "N"
+            cond_owner_record = df['SELLER NAME1'] == 'OWNER RECORD'
+
+            # check the dist. of 0 sale amount
+            df['is_new'] = cond_is_new
+            df['amt_is_nan'] = pd.isna(df['SALE AMOUNT'])
+            tmp = df.groupby(['is_new', 'amt_is_nan']).size().reset_index(name='counts')
+            print("#####")
+            print(tmp)
+            print("=====")
+
+            df['is_owner'] = cond_owner_record
+            tmp = df.groupby(['is_new', 'is_owner']).size().reset_index(name='counts')
+            print("#####")
+            print(tmp)
+            print("=====")
+
+            tmp = df.groupby(['is_new', 'PROPERTY_INDICATOR']).size().reset_index(name='counts')
+            print(tmp)
+
+        txt_extention = ".txt"
+
+        if not files:
+            for f in os.listdir(self._filepath):
+                if f.endswith(txt_extention):
+                    # do the work
+                    print(f)
+        elif isinstance(files, list):
+            # loop through "files"
+            for f in files:
+                if not f.endswith(txt_extention):
+                    f = f + txt_extention
+                
+                tmp_df = self._read_n_clean(
+                    filename=f,
+                    cols_to_keep=cols_to_keep
+                )
+                simple_analysis(tmp_df)
+        else:
+            print(files)
+
+        return
+
+    def deed_peep(self):
         '''
         The actions done here will be based on the data type of self.__filename
         str: does on single file
@@ -29,71 +127,92 @@ class Preprocess():
         None: all of the files in the directory
         '''
 
-        df = pd.read_csv(self._filename, delimiter="|")
+        filepath = "../Corelogic/bulk_deed_fips_split/"
+
+        filename = filepath+'fips-01001-UniversityofPA_Bulk_Deed.txt'
+
+        df = pd.read_csv(filename, delimiter="|")
 
         cols_to_keep = [
+            "PCL_ID_IRIS_FRMTD", # unique key
+            "OWNER_1_LAST_NAME", # empty if is company
             "OWNER_1_FIRST_NAME&MI", # company name
+            "BLOCK LEVEL LATITUDE",
+            "BLOCK LEVEL LONGITUDE",
+            "SITUS_CITY",
+            "SITUS_STATE",
+            "SITUS_ZIP_CODE",
             "SELLER NAME1",
             "SALE AMOUNT", # but after skimming throught, there are several NULL
-            "CORPORATE_INDICATOR",
-            "OWNER_RELATIONSHIP_RIGHTS_CODE",
+            "SALE DATE",
+            "RECORDING DATE",
+            "PROPERTY_INDICATOR", # residential, commercial, ...
+            "OWNER_RELATIONSHIP_RIGHTS_CODE", # not sure will keep this column
             "RESALE/NEW_CONSTRUCTION" # M: re-sale, N: new construction
         ]
         df = df[cols_to_keep]
 
-        cond1 = df['CORPORATE_INDICATOR'] == "Y"
+        cond_is_new = df['RESALE/NEW_CONSTRUCTION'] == "N"
+        cond_owner_record = df['SELLER NAME1'] == 'OWNER RECORD'
 
         # check the dist. of 0 sale amount
-        df['corp_idx'] = cond1
+        df['is_new'] = cond_is_new
         df['amt_is_nan'] = pd.isna(df['SALE AMOUNT'])
-        tmp = df.groupby(['corp_idx', 'amt_is_nan']).size().reset_index(name='counts')
+        tmp = df.groupby(['is_new', 'amt_is_nan']).size().reset_index(name='counts')
         print("#####")
         print(tmp)
         print("=====")
 
-        cond1 = df['CORPORATE_INDICATOR'] == "Y"
+        df['is_owner'] = cond_owner_record
+        tmp = df.groupby(['is_new', 'is_owner']).size().reset_index(name='counts')
+        print("#####")
+        print(tmp)
+        print("=====")
 
-        df = df[cond1]
+        tmp = df.groupby(['is_new', 'PROPERTY_INDICATOR']).size().reset_index(name='counts')
+        print(tmp)
 
-        # for given cond, see the content of owner name
-        # tmp = df['OWNER_1_FIRST_NAME&MI']
-        # print(tmp)
-        # # resale and new constr. dist.
-        # tmp = df.groupby(['RESALE/NEW_CONSTRUCTION']).size().reset_index(name='counts')
-        # print(tmp)
-        # # check what owner rights code might actually means
+        # df = df[cond1]
+
+        # check what owner rights code might actually means
         # tmp = df.groupby(['OWNER_RELATIONSHIP_RIGHTS_CODE']).size().reset_index(name='counts')
         # print(tmp)
-        # # check what are the seller names
-        # tmp = df.groupby(['SELLER NAME1']).size().reset_index(name='counts')
-        # print(tmp)
 
-        # print(df[df['SELLER NAME1'] != 'OWNER RECORD'][['SELLER NAME1', 'OWNER_1_FIRST_NAME&MI']])
-        # print(df[df['SELLER NAME1'] == 'OWNER RECORD'][['SELLER NAME1', 'OWNER_1_FIRST_NAME&MI']])
+        col_to_see = ['SELLER NAME1', 'OWNER_1_FIRST_NAME&MI']
+
+        # print("check owner vs seller name")
+        # print(df[df['SELLER NAME1'] != 'OWNER RECORD'][col_to_see])
+        # print(df[df['SELLER NAME1'] == 'OWNER RECORD'][col_to_see]])
         return
 
-    def check_company_list(self):
+    def __check_company_list(self):
         '''
+        Don't run this, the file is 10 GB, will freeze your PC.
+
         This method simply serves as a exploratory analysis of 
         the company list provided by previous research.
         '''
         filepath = "../Merge_Compustat&Corelogic/"
         filename = filepath+'corelogic_clean.dta'
         
-        comp_list = pd.read_stata(filename)
+        # comp_list = pd.read_stata(filename)
 
-        print(comp_list.columns)
+        # print(comp_list.columns)
 
 
 def main():
 
     filepath = "../Corelogic/bulk_deed_fips_split/"
 
-    filename = filepath+'fips-01001-UniversityofPA_Bulk_Deed.txt'
-
-    p = Preprocess(filename)
-    #p.check_company_list()
-    # p.deed_analysis()
+    p = Preprocess(filepath)
+    # p.check_company_list()
+    file_list = [
+        "fips-01001-UniversityofPA_Bulk_Deed", 
+        "fips-01003-UniversityofPA_Bulk_Deed", 
+        "fips-01005-UniversityofPA_Bulk_Deed"
+    ]
+    p.deed_files(file_list)
+    # p.deed_peep()
 
 
 if __name__ == "__main__":
