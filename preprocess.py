7be6abc400bae5c5f293ebc5085af2cfa39bab5a
KYK <ken19505@gmail.com>
2024-09-02 16:51:15 +0800

    can read tax files now.

--- preprocess.py
+++ preprocess.py
@@ -8,7 +8,12 @@ import csv
 import warnings
 warnings.filterwarnings("ignore")
 
-
+'''HIGH LEVEL IDEA
+Frist extract columns and stack all the data from the same path
+into one large csv. Then we use dask to do operations on these 
+large data.
+'''
+# TODO: merge the code for operations on TAX and DEED.
 
 PROPERTY_INDICATOR = {
     10: "Single Family Residence / Townhouse",
@@ -40,17 +45,20 @@ PROPERTY_INDICATOR = {
 CHUNK_SIZE = 1000000 # this for Deed files is about 400MB
 
 class Preprocess():
-    def __init__(self, filepath: None, log_path: str = "") -> None:
-        self._filepath = filepath
-        self.__log_path = log_path
+    def __init__(self, log_name: str, log_path: str = "./log/") -> None:
+        self._filepath = ""
+        self.__log_file = log_path + log_name
 
-        if not os.path.exists(self.__log_path):
-            with open(self.__log_path, 'w') as f:
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+
+        if not os.path.exists(self.__log_file):
+            with open(self.__log_file, 'w') as f:
                 pass  # Just create an empty file
 
     def write_log(self, message):
         """Writes a log message with a timestamp to the log file."""
-        with open(self.__log_path, 'a') as f:
+        with open(self.__log_file, 'a') as f:
             f.write(f"{message}\n")
 
     def _read_n_clean(self, filename: str = None, cols_to_keep: list = []) -> pd.DataFrame:
@@ -75,7 +83,7 @@ class Preprocess():
 
         dfs = []
 
-        # 如果我現在對於壞的欄位先忽略，那還有需要試dask嗎
+        # 對於壞的row先忽略
         for chunk in pd.read_csv(
             filename, delimiter="|", 
             chunksize=CHUNK_SIZE, # can only work with engine = c or python
@@ -94,14 +102,81 @@ class Preprocess():
         self.write_log(to_log)
 
         return out_df
-
-    def deed_files(self, files = None):
+    
+    def tax_files(
+            self, files = None, 
+            filepath: str = "../Corelogic/bulk_tax_fips_split/"
+        ) -> pd.DataFrame:
         '''
-        The actions done here will be based on the data type of "files
+        The actions done here will be based on the data type of "files"
         str: on single file
         list: on the list of files
         None: all of the files in the directory
         '''
+        self._filepath = filepath
+        self.write_log("Processed Tax files:")
+        cols_to_keep = [
+            "FIPS_CODE",
+            "PCL_ID_IRIS_FORMATTED", # unique key
+            "BLOCK LEVEL LATITUDE", # just to make sure
+            "BLOCK LEVEL LONGITUDE", # just to make sure
+            "ASSESSED_TOTAL_VALUE",
+            "ASSESSED_LAND_VALUE",
+            "ASSESSED_IMPROVEMENT_VALUE",
+            "MARKET_TOTAL_VALUE",
+            "MARKET_LAND_VALUE",
+            "MARKET_IMPROVEMENT_VALUE",
+            "APPRAISED_TOTAL_VALUE",
+            "APPRAISED_LAND_VALUE",
+            "APPRAISED_IMPROVEMENT_VALUE",
+            "TAX_AMOUNT",
+            "TAX_YEAR",
+            "SELLER_NAME", # just to make sure
+            "SALE_AMOUNT", # just to make sure
+            "SALE_DATE" # just to make sure
+        ]
+
+        txt_extention = ".txt"
+        
+        def get_data(f: str):
+            if not f.endswith(txt_extention):
+                f = f + txt_extention
+            
+            tmp_df = self._read_n_clean(
+                filename=f,
+                cols_to_keep=cols_to_keep
+            )
+
+            return tmp_df
+        
+        fs = [] # get all the file names
+        if not files:
+            for f in os.listdir(self._filepath):
+                if f.endswith(txt_extention):
+                    fs.append(f)
+        elif isinstance(files, list):
+            fs = files
+        else:
+            fs.append(files)
+
+        dataframes = [] # get data from the files
+        for f in tqdm(fs, desc="Tax files"):
+            dataframes.append(get_data(f))
+
+        return pd.concat(dataframes, ignore_index=True)
+
+    def deed_files(
+            self, files = None, 
+            filepath: str = "../Corelogic/bulk_deed_fips_split/"
+        ) -> pd.DataFrame:
+        '''
+        The actions done here will be based on the data type of "files"
+        str: on single file
+        list: on the list of files
+        None: all of the files in the directory
+        '''
+        self._filepath = filepath
+        self.write_log("Processed Deed files:")
         cols_to_keep = [
             "FIPS",
             "PCL_ID_IRIS_FRMTD", # unique key
@@ -118,6 +193,8 @@ class Preprocess():
             "RESALE/NEW_CONSTRUCTION" # M: re-sale, N: new construction
         ]
 
+        txt_extention = ".txt"
+        
         def get_data(f: str):
             if not f.endswith(txt_extention):
                 f = f + txt_extention
@@ -130,31 +207,9 @@ class Preprocess():
             tmp_df = tmp_df[tmp_df['RESALE/NEW_CONSTRUCTION'] == "N"].reset_index(drop=True)
 
             return tmp_df
-
-        def _simple_analysis(df: pd.DataFrame):
-            # TODO: Currently no use, can be deleted.
-            # 這裡用沒有經過篩選過的資料跑才有意義
-            cond_is_new = df['RESALE/NEW_CONSTRUCTION'] == "N"
-            cond_owner_record = df['SELLER NAME1'] == 'OWNER RECORD'
-
-            # check the dist. of 0 sale amount
-            df['is_new'] = cond_is_new
-            df['amt_is_nan'] = pd.isna(df['SALE AMOUNT'])
-            tmp = df.groupby(['is_new', 'amt_is_nan']).size().reset_index(name='counts')
-            print("#####")
-            print(tmp)
-            print("=====")
-
-            df['is_owner'] = cond_owner_record
-            tmp = df.groupby(['is_new', 'is_owner']).size().reset_index(name='counts')
-            print("#####")
-            print(tmp)
-            print("=====")
-
-        txt_extention = ".txt"
+        
         dataframes = []
 
-        # TODO: 預計把所有工作都包到一個nested method裡，所以目前這個get_data只是過渡用
         if not files:
             for f in tqdm(os.listdir(self._filepath), desc="Deed files"):
                 if f.endswith(txt_extention):
@@ -238,9 +293,16 @@ class Preprocess():
         # print(df[df['SELLER NAME1'] == 'OWNER RECORD'][col_to_see]])
         return
     
-    def data_output(self, df: pd.DataFrame, filename: str) -> None:
+    def data_output(self, df: pd.DataFrame, filename: str, out_path: str = "./data/") -> None:
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+        
         if not filename.endswith(".csv"):
             filename = filename + ".csv"
+
+        if out_path not in filename:
+            filename = out_path + filename
+
         df.to_csv(f'{filename}', index=False)
         print(f"{filename} generated.")
         return
@@ -261,29 +323,37 @@ class Preprocess():
 
 
 def main():
-    log_path = "./log/"
-    filepath = "../Corelogic/bulk_deed_fips_split/"
-
-    if not os.path.exists(log_path):
-        os.makedirs(log_path)
-
     current_date = datetime.now().strftime("%m%d")
     log = f'deed_{current_date}.log'
 
-    p = Preprocess(filepath=filepath, log_path=log_path+log)
+    p = Preprocess(log_name=log)
+
+    # ===============
+    # Test Deed Files
+    # ===============
     file_list = [
         "fips-01001-UniversityofPA_Bulk_Deed", 
         "fips-01003-UniversityofPA_Bulk_Deed", 
         "fips-01005-UniversityofPA_Bulk_Deed"
     ]
     # file_list = "fips-01001-UniversityofPA_Bulk_Deed"
-    # 想一下這個產好的資料要吐出來還是當作attribute
-    # 目前覺得吐出來好 (把這個preprocess當工具箱的話應該會是吐出來比較好)
-    data = p.deed_files()
+    filepath = "../Corelogic/bulk_deed_fips_split/"
+    # data = p.deed_files(filepath=filepath) # 目前覺得吐出來好 (把這個preprocess當工具箱的話應該會是吐出來比較好)
     # print(data.shape)
-    p.data_output(data, 'deed_stacked.csv')
-
+    # p.data_output(data, 'deed_stacked.csv')
     # p.deed_peep(data)
+
+    # ==============
+    # Test Tax Files
+    # ==============
+    file_list = [
+        "fips-01001-UniversityofPA_Bulk_Tax", 
+        "fips-01003-UniversityofPA_Bulk_Tax", 
+        "fips-01005-UniversityofPA_Bulk_Tax"
+    ]
+    filepath = "../Corelogic/bulk_tax_fips_split/"
+    data = p.tax_files(files=file_list, filepath=filepath)
+    print(data.shape)
 
 
 if __name__ == "__main__":
