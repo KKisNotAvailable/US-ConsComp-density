f432a1b157b3b061f0b48a2a8cae184d58c2756e
Kai Ke <ken19505@gmail.com>
2024-09-04 01:15:09 +0800

    done the groupby analysis

--- analysis.py
+++ analysis.py
@@ -1,12 +1,25 @@
 import dask.dataframe as dd
+import pandas as pd
 
 class Analysis():
     def __init__(self) -> None:
         pass
 
-    def simple_analysis(self):
-        df = dd.read_csv('data/deed_stacked.csv')
-        print(df.columns)
+    def deed_analysis(self):
+        '''
+        COLUMNS:
+        ['FIPS', 'PCL_ID_IRIS_FRMTD', 'BLOCK LEVEL LATITUDE',
+       'BLOCK LEVEL LONGITUDE', 'SITUS_CITY', 'SITUS_STATE', 'SITUS_ZIP_CODE',
+       'SELLER NAME1', 'SALE AMOUNT', 'SALE DATE', 'RECORDING DATE',
+       'PROPERTY_INDICATOR', 'RESALE/NEW_CONSTRUCTION']
+        '''
+        ddf = dd.read_csv('data/deed_stacked.csv')
+
+        ttl_rows = ddf['FIPS'].shape[0].compute() # but why len(ddf['FIPS']) won't work?
+
+        # check if PCL_ID_IRIS_FRMTD is distinct (it is not, but should it be?)
+        # unique_count = ddf['PCL_ID_IRIS_FRMTD'].nunique().compute()
+        # print(f"PCL_ID_IRIS_FRMTD is{' not' if ttl_rows != unique_count else ''} unique key.")
         
         # check the nan distribution of the following columns
         cols_to_clean = [
@@ -15,27 +28,44 @@ class Analysis():
             "SALE AMOUNT", 
             "SALE DATE"
         ]
-        good_counts = df[cols_to_clean].count().compute()
-        print(good_counts)
-        ttl_rows = len(df['FIPS'])
-        print(ttl_rows)
-
-        # for i, c in enumerate(cols_to_clean):
-        #     print(f"Non NA pct for {c}: {good_counts[i]*100/ttl_rows:.2f}%")
-
-        # ignore rows if SITUS_CITY, SITUS_STATE, SALE AMOUNT, SALE DATE
-        # is empty
+        good_counts = ddf[cols_to_clean].count().compute()
         
-        # df = df.dropna(subset=cols_to_clean)
+        print("Non NA pct for ...")
+        for i, c in enumerate(cols_to_clean):
+            print(f"  {c:<11}: {good_counts.iloc[i]*100/ttl_rows:>7.2f}%")
 
-        # check if PCL_ID_IRIS_FRMTD is distinct
+        # 1. ignore rows if any of those columns is empty
+        ddf = ddf.dropna(subset=cols_to_clean)
 
+        # 2. group by SITUS_CITY / SITUS_STATE 
+        #   2-1. count cases, distinct "SELLER NAME1" 
+        #   2-2. sum "SALE AMOUNT"
+        cols_to_gp = ["SITUS_CITY", "SITUS_STATE"]
 
-        # group by SITUS_CITY / SITUS_STATE 
-        # 1. count cases, distinct SELLER NAME1 
-        # 2. sum "SALE AMOUNT"
+        for c in cols_to_gp:
+            to_cat = [
+                ddf.groupby(c)\
+                    .agg(
+                        case_cnt=('PCL_ID_IRIS_FRMTD', 'count'),
+                        sale_amt=('SALE AMOUNT', 'sum')
+                    )\
+                    .compute(),
+                # since the following is a series, no need to specify column when renaming
+                ddf.groupby(c)['SELLER NAME1']\
+                    .nunique()\
+                    .rename('uniq_seller_cnt')\
+                    .compute()
+            ]
+            
+            result = pd.concat(to_cat, axis=1) # horizontally
+            
+            print(f"Grouping result by {c}\n{result}")
+
+        # plot the non yearly result on maps
 
         # do similar action above but based on SALE YEAR
+        # maybe the plotting can be animated? 
+        # (even is the plotting for yearly possible?)
 
 
 def main():
@@ -45,7 +75,7 @@ def main():
     # =======================
     # Test analysis with dask
     # =======================
-    a.simple_analysis()
+    a.deed_analysis()
 
 if __name__ == "__main__":
     main()
