c954809881f5fc9726ea7d170087375c3f94aea9
Kai Ke <ken19505@gmail.com>
2024-10-04 00:46:58 +0800

    can plot time series now

--- analysis.py
+++ analysis.py
@@ -14,6 +14,10 @@ from matplotlib.patches import Patch, Circle
 edge_color = "#30011E"
 background_color = "#fafafa"
 
+
+
+
+
 sns.set_style({
     "font.family": "serif",
     "figure.facecolor": background_color,
@@ -41,6 +45,9 @@ GEO COLUMNS:
 class Analysis():
     def __init__(self, out_path: str = "./output/") -> None:
         self.__out_path = out_path
+        self.threshold = 20
+        self.sizex = 16
+        self.sizey = 12
 
         if not os.path.isdir(out_path):
             os.mkdir(out_path)
@@ -172,8 +179,15 @@ class Analysis():
             scc_map = scc_map[['state_id', 'county_fips', 'zips']]
             print(scc_map.head(5))
         
+        def check_abbreviation():
+            states = gpd.read_file("data/cb_2018_us_state_500k/")
+            # "Puerto Rico", "American Samoa", "United States Virgin Islands"
+            # "Guam", "Commonwealth of the Northern Mariana Islands"
+            states = states[states.STATEFP.isin(["72", "69", "60", "66", "78"])]
 
-        # check_deed_var_distribution()
+            print(states[['NAME', 'STATEFP', 'STUSPS']])
+
+        # check_abbreviation()
         return
     
     def deed_prep(self, is_yearly=True, gen_data=False):
@@ -203,11 +217,14 @@ class Analysis():
 
         # 1. data prep
         #   1.1 ignore rows if any of those columns is empty
-        #   1.2 fill the 'SALE DATE' when 0 with 'RECORDING DATE'
-        #   1.3 make the 'year' column (opt.)
-        #   1.3 (cont.) ignore the rows with 'year' == 0, 
+        #   1.2 make the STATEFP column, since there are some typo of SITUS_STATE
+        #   1.3 fill the 'SALE DATE' when 0 with 'RECORDING DATE'
+        #   1.4 make the 'year' column (opt.)
+        #   1.4 (cont.) ignore the rows with 'year' == 0, 
         #       should be around 5831 after combining SALE and RECORDING
         ddf = ddf.dropna(subset=cols_to_clean)
+        ddf['STATEFP'] = ddf['FIPS'].astype(str).str.zfill(5).str[:2]
+
         ddf['SALE DATE'] = ddf['SALE DATE'].mask(ddf['SALE DATE'] == 0, ddf['RECORDING DATE'])
 
         if is_yearly:
@@ -217,13 +234,13 @@ class Analysis():
         else:
             pre_group_base = []
 
-        # 2. group by year, SITUS_STATE, FIPS
+        # 2. group by year, STATEFP, FIPS
         #   2-1. count cases
         #   2-2. sum "SALE AMOUNT"
         
         grouped_results = {
             "FIPS": None, 
-            "SITUS_STATE": None
+            "STATEFP": None
         }
 
         for c in grouped_results.keys():
@@ -283,6 +300,18 @@ class Analysis():
             grouped_results[c] = pd.concat(to_cat, axis=1).reset_index()\
                 .sort_values(by=group_base, ascending=[True] * len(group_base))
             
+            # give the states their abbreviation
+            if c == 'STATEFP':
+                states = gpd.read_file("data/cb_2018_us_state_500k/")
+                states = states[['STUSPS', 'STATEFP']]
+
+                tmp = pd.merge(grouped_results[c], states, on='STATEFP', how='left')
+                tmp = tmp[['STUSPS'] + [col for col in tmp.columns if col != 'STUSPS']]
+                if is_yearly:
+                    tmp = tmp[['year'] + [col for col in tmp.columns if col != 'year']]
+
+                grouped_results[c] = tmp
+            
         if gen_data:
             for c, res in grouped_results.items():
                 fname = f"agg_result_{c}.csv"
@@ -294,7 +323,7 @@ class Analysis():
         return
 
     
-    def deed_plot(self, filenames, save_fig=False, hhi_base='sale_amt'):
+    def deed_plot_heat_map(self, filename, scale='states', hhi_base='sale_amt', save_fig=False):
         '''
         REF: https://dev.to/oscarleo/how-to-create-data-maps-of-the-united-states-with-matplotlib-p9i
         plot the non yearly result on maps
@@ -306,7 +335,11 @@ class Analysis():
         hhi_base: 'sale_amt' or 'case_cnt'
         '''
         if not hhi_base in ['sale_amt', 'case_cnt']:
-            print("Please provide a valid HHI base, either sale_amt or case_cnt")
+            print("Please provide a valid HHI base, either 'sale_amt' or 'case_cnt'")
+            return
+        
+        if not scale in ['states', 'counties']:
+            print("Please state a valid scale, either 'states' or 'counties'")
             return
 
         def translate_geometries(df, x, y, scale, rotate):
@@ -326,26 +359,26 @@ class Analysis():
 
             return pd.concat([df_main_land, df_alaska, df_hawaii])
 
-        if not isinstance(filenames, list):
-            filenames = [filenames]
+        f = self.__out_path + filename # TODO: should change the way of writing this
+        HHI = pd.read_csv(f) # or maybe we can combine the scale indicator with the filename
 
-        for f in filenames:
-            f = self.__out_path + f
-            HHI = pd.read_csv(f)
-            cur_hhi = 'HHI_sale_amt' if hhi_base == 'sale_amt' else 'HHI_case_cnt'
-            keep_cols = ['SITUS_STATE', cur_hhi, 'case_cnt']
-            HHI = HHI[keep_cols]
-            HHI.rename(
-                columns={
-                    'SITUS_STATE': 'STUSPS',
-                    cur_hhi: 'value'
-                }, 
-                inplace=True
-            )
+        cur_hhi = 'HHI_sale_amt' if hhi_base == 'sale_amt' else 'HHI_case_cnt'
+        cur_scale = 'STUSPS' if scale == 'states' else 'FIPS'
+        scale_new_name = 'STUSPS' if scale == 'states' else 'GEOID'
+
+        keep_cols = [cur_scale, cur_hhi, 'case_cnt']
+        HHI = HHI[keep_cols]
+        HHI.rename(
+            columns={
+                cur_scale: scale_new_name,
+                cur_hhi: 'HHI'
+            }, 
+            inplace=True
+        )
 
         counties = gpd.read_file("data/cb_2018_us_county_500k/")
         counties = counties[~counties.STATEFP.isin(["72", "69", "60", "66", "78"])]
-        counties = counties.set_index("GEOID")
+        counties["GEOID"] = counties["GEOID"].astype(int)
 
         states = gpd.read_file("data/cb_2018_us_state_500k/")
         # remove "unincorporated territories":
@@ -362,7 +395,7 @@ class Analysis():
         states = adjust_maps(states)
 
         # add data with color to the states df.
-        low_case_cnt_color = "grey"
+        low_case_cnt_color = "lightgrey"
         data_breaks = [
             (90, "#ff0000", "Top 10%"),   # Bright Red
             (70, "#ff4d4d", "90-70%"),    # Light Red
@@ -371,39 +404,90 @@ class Analysis():
             (0,  "#ffe6e6", "Bottom 30%") # Very Light Red
         ]
 
-        def create_color(county_df, data_breaks):
+        def create_color(county_df, data_breaks, target):
             colors = []
 
             for i, row in county_df.iterrows():
                 for p, c, _ in data_breaks:
-                    if row.value >= np.percentile(county_df.value, p):
+                    if row[target] >= np.percentile(county_df[target], p):
                         colors.append(c)
                         break
 
             return colors
         
-        states = pd.merge(states, HHI, on='STUSPS', how='left')
-        states.loc[:, "color"] = create_color(states, data_breaks)
+        cur_geo = states if scale == 'states' else counties
+        
+        to_plot = pd.merge(cur_geo, HHI, on=scale_new_name, how='left')
+        # since there will be some counties that has no HHI data, we fill them with 0
+        to_plot[['HHI', 'case_cnt']] = to_plot[['HHI', 'case_cnt']].fillna(0)
+        to_plot.loc[:, "color"] = create_color(to_plot, data_breaks, target='HHI')
 
         # if 'case_cnt' is smaller than threshold, we set its color to 'low_case_cnt_color'
-        threshold = 30
-        states.loc[states['case_cnt'] < threshold, 'color'] = low_case_cnt_color
+        to_plot.loc[to_plot['case_cnt'] < self.threshold, 'color'] = low_case_cnt_color
 
-        sizex = 8
-        sizey = 6
-        ax = counties.plot(edgecolor=edge_color + "55", color="None", figsize=(sizex, sizey))
-        states.plot(ax=ax, edgecolor=edge_color, color=states.color, linewidth=1)
+        ax = counties.plot(edgecolor=edge_color + "55", color="None", figsize=(self.sizex, self.sizey))
+        to_plot.plot(ax=ax, edgecolor=edge_color, color=to_plot.color, linewidth=0.5)
 
         plt.axis("off")
 
         if save_fig:
-            pass
-            # do the saving thing
+            plt.savefig(f"{self.__out_path+scale}.svg", format="svg")
         else:
             plt.show()
-
         return
     
+    def deed_plot_time_series(self, filename, hhi_base='sale_amt', save_fig=False):
+        '''
+        This function only considers plotting for all the states, 
+        since including all the counties in one plot would be chaos.
+
+        Also this function might also support plotting for a given state or county. 
+        (the corresponding data shold be provided though)
+        '''
+        # TODO: might want to make this hhs_base as the class attribute
+        if not hhi_base in ['sale_amt', 'case_cnt']:
+            print("Please provide a valid HHI base, either 'sale_amt' or 'case_cnt'")
+            return
+
+        f = self.__out_path + filename
+        df = pd.read_csv(f)
+
+        df = df[df['year'] >= 1987] # since 1987 差不多HHI趨於穩定，也是FRED資料庫S&P CoreLogic Case-Shiller U.S. National Home Price Index 的起始點
+        df = df[df['case_cnt'] >= self.threshold]
+        cur_hhi = 'HHI_sale_amt' if hhi_base == 'sale_amt' else 'HHI_case_cnt'
+
+        # Define the full range of years expected in the data
+        full_years = np.arange(df['year'].min(), df['year'].max() + 1)
+
+        fig, ax = plt.subplots(figsize=(self.sizex, self.sizey))
+
+        n_color = len(set(df['STUSPS']))
+        palette = sns.color_palette("icefire", n_colors=n_color)  # distinct colors
+        state_colors = {state: palette[i] for i, state in enumerate(df['STUSPS'].unique())}
+
+        # Group by state and plot each group separately
+        for state, group in df.groupby('STUSPS'):
+            # Reindex to include all years, filling missing HHI with NaN
+            group = group.set_index('year').reindex(full_years).reset_index()
+            group['state'] = state  # Refill the state column
+            group[cur_hhi] = group[cur_hhi].interpolate()  # Interpolate missing HHI values
+
+            # Plot the reindexed and interpolated data
+            ax.plot(group['year'], group[cur_hhi], label=state, marker='o', color=state_colors[state])
+
+        # Add plot details
+        ax.set_xlabel('Year')
+        ax.set_ylabel('HHI')
+        ax.set_title('HHI Over Time by State')
+        ax.legend(title='State', loc='upper right', ncol=3, fontsize='small', frameon=True)  # Place legend outside
+        plt.grid()
+
+        if save_fig:
+            plt.savefig(f"{self.__out_path}state_scatter.svg", format="svg")
+        else:
+            plt.show()
+        return
+
     def file_out(self, df, filename: str) -> None:
         # TODO: filename validity check
         # TODO: kwargs for to_csv
@@ -431,17 +515,28 @@ def main():
     # a.deed_prep(is_yearly=False, gen_data=True)
 
     # ==========
-    #  plotting
+    #  Plotting
     # ==========
-    # TODO: check 那些太少的州真的在地圖上對的到嗎 & 那些太少的州叫啥名
-    files = ['agg_result_SITUS_STATE.csv'] # 'agg_result_FIPS.csv'
-    a.deed_plot(files, save_fig=False, hhi_base='sale_amt')
+    # 1. draw heat map
+    tb_list = {
+        'states': 'agg_result_STATEFP.csv',
+        'counties': 'agg_result_FIPS.csv'
+    }
+    # for s, file in tb_list.items():
+    #     a.deed_plot_heat_map(file, save_fig=True, hhi_base='sale_amt', scale=s)
+
+    # 2. draw time series of states from 1987
+    file = 'yearly_agg_result_STATEFP.csv'
+    a.deed_plot_time_series(file, hhi_base='sale_amt', save_fig=True)
+
+    # 3. scatter plot of HHI (x-axis) and price change (y-axis)
+    
 
     # ===============
     #  Wild Thoughts
     # ===============
     # so I'm thinking maybe we could do the animation of yearly HHI heatmap
-    # change since some 1970s?
+    # change since some 1987?
 
 if __name__ == "__main__":
     main()
